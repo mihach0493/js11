c3732749f784a58a26a0c2126d94b277bf1b2130
Mykhailo Chui <mihach0493@gmail>
2020-09-13 10:02:04 +0200

    js11

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstNumber = 1;
+let secondNumber = 2;
+
+function multiply(firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+    return result;
+}
+
+console.log(multiply(1, 2));
